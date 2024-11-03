bd88418358098cdc0b3f4c096c7732540637616b
sasiska <vzuravlev20@gmail.com>
2024-11-04 02:25:10 +0300

    add table with prices
    add test price view

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from typing import Union
 import requests
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from settings import POLYGON_API_KEY
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get('/')
@@ -27,10 +30,34 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 
 @app.get("/prices")
-def get_prices():
+def get_prices(request: Request):
+    time = 15
     url = (
-        f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&apikey={POLYGON_API_KEY}"
+        f"https://www.alphavantage.co/query?"
+        f"function=TIME_SERIES_INTRADAY&"
+        f"symbol=AAPL&"
+        f"interval={time}min&"
+        f"month=2024-11&"
+        f"outputsize=compact&"
+        f"apikey={POLYGON_API_KEY}"
     )
     r = requests.get(url)
     data = r.json()
-    return data
+    print(data)
+    return templates.TemplateResponse(
+        name="prices.html",
+        context={
+            "data": data
+        },
+        request=request
+    )
+
+    # return data
+
+
+@app.get("/test")
+def test(request: Request):
+    return templates.TemplateResponse(
+        name="test_template/index.html",
+        request=request,
+    )
